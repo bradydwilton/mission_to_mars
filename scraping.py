590663509839c936bf6ed6f2708ae200811701e6
bradydwilton <bradydwilton@gmail.com>
2021-02-24 20:28:23 -0600

    10.5.2 commit

--- scraping.py
+++ scraping.py
@@ -1,65 +1,103 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
 import pandas as pd
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-
-# Create executable path for chromedriver
-executable_path = {'executable_path': 'chromedriver.exe'}
-browser = Browser('chrome', **executable_path, headless=False)
+from datetime import datetime as dt
 
 
-# Scrape Mars News Titles & Summaries
+def scrape_all():
 
-# Visit mars.nasa.gov
-news_url = 'https://mars.nasa.gov/news/'
-browser.visit(news_url)
+    # Create executable path for chromedriver
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=True)
 
-# Delay to allow page to fully load (optional)
-browser.is_element_present_by_css('ul.item_list li.slide', wait_time=10)
+    # store news variables
+    news_title, news_p = mars_news(browser)
 
-# Create parent element for most recent news story
-slide_elem = soup(browser.html, 'html.parser').select_one(
-    'ul.item_list li.slide')
+    # store data in dictionary
+    data = {
+        'news_title': news_title,
+        'news_paragraph': news_p,
+        'featured_image': featured_image(browser),
+        'facts': mars_facts(browser),
+        'last_modified': dt.now()
+    }
 
-# Get title & summary from parent element
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_p = slide_elem.find('div', class_='article_teaser_body').text
+    # stop webdriver & return data
+    browser.quit()
+    return data
 
 
-# Scrape featured image
+def mars_news(browser):
 
-# Visit featured image website
-images_url = 'http://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
-browser.visit(images_url)
+    # Scrape Mars News Titles & Summaries
 
-# Click 'Full Image' button on homepage of site
-browser.click_link_by_partial_text('FULL IMAGE')
+    # Visit mars.nasa.gov
+    news_url = 'https://mars.nasa.gov/news/'
+    browser.visit(news_url)
 
-# Save image url
-img_link_rel = soup(browser.html, 'html.parser').find(
-    'img', class_='fancybox-image').get('src')
-img_url = f'http://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_link_rel}'
+    # Delay to allow page to fully load (optional)
+    browser.is_element_present_by_css('ul.item_list li.slide', wait_time=1)
 
-# Scrape Mars facts
+    try:
+        # Create parent element for most recent news story
+        slide_elem = soup(browser.html, 'html.parser').select_one(
+            'ul.item_list li.slide')
 
-# Visit space-facts.com
-facts_url = 'https://space-facts.com/mars/'
-# browser.visit(facts_url)
+        # Get title & summary from parent element
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+        news_p = slide_elem.find('div', class_='article_teaser_body').text
 
-# Store Mars facts table into DataFrame
-df = pd.read_html('https://space-facts.com/mars/')[0]
-df.columns = ['description', 'value']
-df.set_index('description', inplace=True)
-df
+    # add exception for error caused by webpage changing format
+    except AttributeError:
+        return None, None
 
-# Convert dataframe to html
-df.to_html()
+    return news_title, news_p
 
-# exit browser
-browser.quit()
 
-# print(news_title)
-# print(news_p)
-# print(img_url)
-# print(df)
+def featured_image(browser):
+
+    # Scrape featured image
+
+    # Visit featured image website
+    images_url = 'http://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(images_url)
+
+    # Click 'Full Image' button on homepage of site
+    browser.click_link_by_partial_text('FULL IMAGE')
+
+    try:
+        # Save image url
+        img_link_rel = soup(browser.html, 'html.parser').find(
+            'img', class_='fancybox-image').get('src')
+        img_url = f'http://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_link_rel}'
+
+    except AttributeError:
+        return None
+
+    return img_url
+
+
+def mars_facts(browser):
+
+    # Scrape Mars facts
+
+    # Visit space-facts.com
+    facts_url = 'https://space-facts.com/mars/'
+    # browser.visit(facts_url)
+
+    try:
+        # Store Mars facts table into DataFrame
+        df = pd.read_html(facts_url)[0]
+        df.columns = ['description', 'value']
+        df.set_index('description', inplace=True)
+        df
+    except BaseException:
+        return None
+
+    # Convert dataframe to html
+    return df.to_html()
+
+
+if __name__ == '__main__':
+    # If running as script, print scraped data
+    print(scrape_all())
